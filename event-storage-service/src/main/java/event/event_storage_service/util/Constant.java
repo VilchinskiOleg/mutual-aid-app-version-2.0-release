8db7ab7babd058fb9f47b8a811781bbc7797c788
oleg <vilchinskioleg@gmail.com>
2021-10-23 17:08:34 +0300

    PS-0016: improvement consumer.

--- event-storage-service/src/main/java/event/event_storage_service/util/Constant.java
+++ event-storage-service/src/main/java/event/event_storage_service/util/Constant.java
@@ -10,8 +10,13 @@ public class Constant {
     public static class Kafka {
 
         //default value : 300000
-        public static final Integer MAX_POLL_INTERVAL_MS = 900000;
+        public static final Integer MAX_POLL_INTERVAL_MS = 3000000;
         //default value : 500
         public static final Integer MAX_POLL_RECORDS = 100;
+        //auto.offset.reset config, default value : latest
+        public static final String LATEST = "latest";
+        public static final String EARLIEST = "earliest";
+
+        public static final String UPDATE_ORDER_EVENT = "update_order_event";
     }
 }
