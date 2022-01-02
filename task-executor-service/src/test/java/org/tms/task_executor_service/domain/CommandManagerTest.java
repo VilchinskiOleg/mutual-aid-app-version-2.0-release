da8951ed100ba854b15bfe0327fea9593a9692a9
aleh.vilchynski <aleh.vilchynski@resliv.by>
2022-01-02 19:30:55 +0300

    PS-0018: add test

---
+++ task-executor-service/src/test/java/org/tms/task_executor_service/domain/CommandManagerTest.java
@@ -0,0 +1,60 @@
+package org.tms.task_executor_service.domain;
+
+import static java.time.LocalDateTime.now;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+import static org.mockito.Mockito.spy;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.tms.task_executor_service.domain.model.Task.Type.CREATE_PROFILE;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mapper.autoconfiguration.mapper.Mapper;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.tms.task_executor_service.domain.model.Task;
+import org.tms.task_executor_service.domain.model.payload.CreateProfilePayload;
+import org.tms.task_executor_service.domain.service.CommandManager;
+import org.tms.task_executor_service.domain.service.client.ProfileClientService;
+import org.tms.task_executor_service.domain.service.client.TaskExecutionProvider;
+import org.tms.task_executor_service.domain.service.command.Command;
+
+@TestInstance(PER_CLASS)
+public class CommandManagerTest {
+
+    @InjectMocks
+    private CommandManager commandManager;
+    @Spy
+    private final List<TaskExecutionProvider> executionProviders = new ArrayList<>();
+    @InjectMocks
+    private final TaskExecutionProvider profileTaskExecutionProvider = spy(ProfileClientService.class);
+    @Mock
+    private Mapper mapper;
+
+    @BeforeAll
+    void initAll() {
+        initMocks(this);
+        executionProviders.add(profileTaskExecutionProvider);
+    }
+
+    @Test
+    void retrieve_command_if_found_execution_provider_for_it() {
+        Task createProfileTask = getCreateProfileTask();
+        Command command = commandManager.retrieveCommand(createProfileTask);
+        assertEquals(CREATE_PROFILE.getCommandImplClass().getSimpleName(), command.getName());
+    }
+
+    private Task getCreateProfileTask() {
+        Task task = new Task();
+        task.setInternalId("internalId");
+        task.setType(CREATE_PROFILE);
+        task.setCreatedAt(now());
+        CreateProfilePayload payload = new CreateProfilePayload();
+        task.setPayload(payload);
+        return task;
+    }
+}
