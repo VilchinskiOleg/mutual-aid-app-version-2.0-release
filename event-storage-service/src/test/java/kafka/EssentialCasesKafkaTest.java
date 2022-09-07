35436fce65734e8adcfa0f5cd04c01665765e8b1
VilchinskiOleg <vilchinskioleg@gmail.com>
2022-09-07 22:15:14 +0300

    PS-0021: update Kafka tests. Now they work with Testcontainer.

--- event-storage-service/src/test/java/kafka/EssentialCasesKafkaTest.java
+++ event-storage-service/src/test/java/kafka/EssentialCasesKafkaTest.java
@@ -14,6 +14,7 @@ import event.event_storage_service.domain.service.processor.IdGeneratorService;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,26 +24,41 @@ import javax.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @Slf4j
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {IdGeneratorService.class})
+
+@Testcontainers
+@DirtiesContext
 public class EssentialCasesKafkaTest extends BaseKafkaComponentTest {
+
+  @Container
+  static KafkaContainer kafkaContainer =
+      new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:6.2.1"));
 
   private final Map<Pair<String, String>, ConsumerRecord<String, KafkaOrderEvent>> RESULT_PUBLISHING_MAP = new HashMap();
   @Resource
   private IdGeneratorService idGenerator;
 
   {
-    setupAndInitialKafkaTemplate(null);
+    setupAndInitialKafkaTemplate(Map.of(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers()));
   }
 
   @BeforeEach
@@ -62,11 +78,24 @@ public class EssentialCasesKafkaTest extends BaseKafkaComponentTest {
     final String group_B = "GroupB";
     final String topicName = "TestTopicZero";
 
+    NewTopic topic =  TopicBuilder.name(topicName).partitions(3).replicas(1).build();
+    try (var admin = AdminClient.create(Map.of(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers()))) {
+      admin.createTopics(Collections.singletonList(topic));
+    }
+
     // Set up consumer A:
-    final String consumerA = this.setupAndStartContainer(group_A, topicName, null);
+    final String consumerA = this.setupAndStartContainer(
+        group_A,
+        topicName,
+        Map.of(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers())
+    );
 
     // Set up consumer B:
-    final String consumerB = this.setupAndStartContainer(group_B, topicName, null);
+    final String consumerB = this.setupAndStartContainer(
+        group_B,
+        topicName,
+        Map.of(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers())
+    );
 
     // Publish messages and waite result:
     publishEventsAndWaiteResult(
@@ -103,11 +132,24 @@ public class EssentialCasesKafkaTest extends BaseKafkaComponentTest {
     final String group = "CommonGroup";
     final String topicName = "TestTopicFirst";
 
+    NewTopic topic =  TopicBuilder.name(topicName).partitions(3).replicas(1).build();
+    try (var admin = AdminClient.create(Map.of(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers()))) {
+      admin.createTopics(Collections.singletonList(topic));
+    }
+
     // Set up consumer A, it got uniq ID, but will work for the same topic and group like B:
-    final String consumerA = this.setupAndStartContainer(group, topicName, null);
+    final String consumerA = this.setupAndStartContainer(
+        group,
+        topicName,
+        Map.of(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers())
+    );
 
     // Set up consumer B, it got uniq ID, but will work for the same topic and group like A:
-    final String consumerB = this.setupAndStartContainer(group, topicName, null);
+    final String consumerB = this.setupAndStartContainer(
+        group,
+        topicName,
+        Map.of(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers())
+    );
 
     // Publish messages and waite result:
     publishEventsAndWaiteResult(
@@ -176,7 +218,7 @@ public class EssentialCasesKafkaTest extends BaseKafkaComponentTest {
     stopContainers();
   }
 
-  public KafkaOrderEvent getMockedOrderEvent(String fileName) {
+  private KafkaOrderEvent getMockedOrderEvent(String fileName) {
     URL url = ClassLoader.getSystemClassLoader()
         .getResource("mockdata/".concat(fileName));
     if (nonNull(url)) {
