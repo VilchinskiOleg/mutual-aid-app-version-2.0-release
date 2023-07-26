1d1dc45d8db3eec12efbf97bf60f6cf59a4b1dd7
aleh.vilchynski <vilchinskioleg@gmail.com>
2023-07-26 12:03:15 +0200

    commit 2 (wrong).

--- message-chat-service/src/main/java/messagechat/messagechatservice/mapper/DataDialogToDialogConverter.java
+++ message-chat-service/src/main/java/messagechat/messagechatservice/mapper/DataDialogToDialogConverter.java
@@ -1,14 +1,15 @@
 package messagechat.messagechatservice.mapper;
 
-import static org.springframework.util.CollectionUtils.isEmpty;
-import static messagechat.messagechatservice.domain.model.Dialog.Status;
-import static messagechat.messagechatservice.domain.model.Dialog.Type;
-
 import messagechat.messagechatservice.domain.model.Member;
 import messagechat.messagechatservice.persistent.entity.Dialog;
 import org.mapper.autoconfiguration.converter.BaseConverter;
 import org.springframework.stereotype.Component;
+
 import java.util.HashSet;
+
+import static messagechat.messagechatservice.domain.model.Dialog.Status;
+import static messagechat.messagechatservice.domain.model.Dialog.Type;
+import static org.springframework.util.CollectionUtils.isEmpty;
 
 @Component
 public class DataDialogToDialogConverter extends BaseConverter<Dialog, messagechat.messagechatservice.domain.model.Dialog> {
@@ -28,8 +29,8 @@ public class DataDialogToDialogConverter extends BaseConverter<Dialog, messagech
         destination.setStatus(mapper.map(source.getStatus(), Status.class));
         destination.setType(mapper.map(source.getType(), Type.class));
         destination.setCreateAt(source.getCreateAt());
-        destination.setModifyAt(source.getModifyAt());
-        destination.setCreateByMemberId(source.getCreateByMemberId());
-        destination.setModifyByMemberId(source.getModifyByMemberId());
+//        destination.setModifyAt(source.getModifyAt());
+//        destination.setCreateByMemberId(source.getCreateByMemberId());
+//        destination.setModifyByMemberId(source.getModifyByMemberId());
     }
 }
