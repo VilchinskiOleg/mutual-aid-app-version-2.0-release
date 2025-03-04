184407f35d9f5e3c8db034b8c1750a872019963a
aleh.vilchynski <vilchinskioleg@gmail.com>
2025-03-04 23:58:39 +0100

    Adjusted Transaction Isolation/Lock tests for Hibernet. Reproduced list of popular cases.

--- message-chat-service/src/test/java/messagechat/messagechatservice/data/core/IsolationTransactionTest.java
+++ message-chat-service/src/test/java/messagechat/messagechatservice/data/core/IsolationTransactionTest.java
@@ -6,9 +6,8 @@ import messagechat.messagechatservice.configuration.data.MessageChatJpaConfig;
 import messagechat.messagechatservice.data.common.AbstractTest;
 import messagechat.messagechatservice.domain.service.proessor.CacheManagerImpl;
 import messagechat.messagechatservice.persistent.entity.Dialog;
-import messagechat.messagechatservice.persistent.entity.Message;
-import org.hibernate.LockMode;
-import org.hibernate.LockOptions;
+import messagechat.messagechatservice.persistent.entity.Member;
+import org.hibernate.PessimisticLockException;
 import org.hibernate.Session;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -21,11 +20,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.annotation.Resource;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.LockModeType;
-import javax.persistence.OptimisticLockException;
-import javax.persistence.RollbackException;
+import javax.persistence.*;
+import java.util.Map;
 
+import static org.hibernate.cfg.AvailableSettings.JPA_LOCK_TIMEOUT;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles("local")
@@ -38,6 +36,9 @@ public class IsolationTransactionTest extends AbstractTest {
     private static final String DIALOG_ID = "test-dialog-1";
     private static final String MESSAGE_ID = "test-message-1";
 
+    private static final String F_D_CH = "Dialog Name - First update";
+    private static final String S_D_CH = "Dialog Name - Second update";
+
     @Resource
     private EntityManagerFactory entityManagerFactory;
 
@@ -47,13 +48,15 @@ public class IsolationTransactionTest extends AbstractTest {
 
 
     /**
-     * Prevent Last Commit wins effect by Optimistic Lock:
+     * Optimistic Lock :
+     * Prevent "Last Commit wins" effect using Optimistic Lock.
+     *
+     * No inner Entities.
      */
     @Test
-    void throw_exception_by_optimistic_lock_when_try_to_change_dialog_name() {
+    void throwException_byOptimisticLock_whenTryToChangeDialogName() {
         createDialogForCouple((Session) entityManagerFactory.createEntityManager(), DIALOG_ID);
 
-        final String F_D_CH = "First dialog NAME changing";
         @Cleanup final Session session1 = (Session) entityManagerFactory.createEntityManager();
         @Cleanup final Session session2 = (Session) entityManagerFactory.createEntityManager();
 
@@ -68,9 +71,10 @@ public class IsolationTransactionTest extends AbstractTest {
 
             //User 2:
             var theSamedialog = session2.find(Dialog.class, 1, LockModeType.OPTIMISTIC);
-            theSamedialog.setName("Second dialog NAME changing");
+            theSamedialog.setName(S_D_CH);
 
-            //Thanks to Optimistic Locking -> First commit win:
+            // Thanks to Optimistic Locking, First Commit will win.
+            // Changes from second session will be ignored and app will throw ex because of that :
             session1.getTransaction().commit();
             session2.getTransaction().commit();
         });
@@ -81,54 +85,231 @@ public class IsolationTransactionTest extends AbstractTest {
         cleanDb((Session) entityManagerFactory.createEntityManager());
     }
 
+    /**
+     * Optimistic Lock :
+     * Prevent "Last Commit wins" effect using Optimistic Lock.
+     *
+     * Fetch inner Entities.
+     */
     @Test
-    @Disabled //TODO: Remove annotation after fixing issue with deadlock.
-    void pessimistic_lock_test() {
+    @Disabled
+    // todo: fix "org.hibernate.HibernateException: Unable to perform beforeTransactionCompletion callback: null"
+    void throwException_byOptimisticLock_whenTryToChangeDialogMemberData_withInnerEntity() {
         createDialogForCouple((Session) entityManagerFactory.createEntityManager(), DIALOG_ID);
 
-        final String F_M_CH = "First message DESCRIPTION changing";
+        //validate:
+        Exception ex = assertThrows(RollbackException.class, () -> {
+            try (Session session1 = (Session) entityManagerFactory.createEntityManager();
+                 Session session2 = (Session) entityManagerFactory.createEntityManager()) {
+
+                session1.beginTransaction();
+                session2.beginTransaction();
+
+                // U1:
+                var dialog = session1.createQuery(
+                                "select d from Dialog d " +
+                                        "join fetch d.dialogByMemberDetails dm " +
+                                        "join fetch dm.member m " +
+//                                    "join fetch m.memberInfo mi " +
+                                        "where d.id = :id", Dialog.class)
+                        .setParameter("id", 1)
+                        .setLockMode(LockModeType.OPTIMISTIC)
+                        .getSingleResult();
+                dialog.setName(F_D_CH);
+
+                // U2:
+                var theSameDialog = session2.createQuery(
+                                "select d from Dialog d " +
+                                        "join fetch d.dialogByMemberDetails dm " +
+                                        "join fetch dm.member m " +
+//                                    "join fetch m.memberInfo mi " +
+                                        "where d.id = :id", Dialog.class)
+                        .setParameter("id", 1)
+                        .setLockMode(LockModeType.OPTIMISTIC)
+                        .getSingleResult();
+                theSameDialog.setName(S_D_CH);
+
+                // предролагаю что в рамках диалога апдейт не пройдет из-за последовательности каскадных операций,
+                // но если прапдейтить того-же участника отдельно - изменения попадут в БД, т.к. у него нет версии..
+                var firstMember = theSameDialog.getMembers().get(0);
+                firstMember.setProfileId("Changed ProfileId ..");
+
+                // Thanks to Optimistic Locking, First Commit will win.
+                // Changes from second session will be ignored and app will throw ex because of that :
+                session1.getTransaction().commit();
+                session2.getTransaction().commit();
+            }
+        });
+//        assertTrue(ex.getCause() instanceof OptimisticLockException);
+        var dialog = readDialogByDialogIdWithFetchedMembers((Session) entityManagerFactory.createEntityManager(), DIALOG_ID);
+        assertEquals(F_D_CH, dialog.getName());
+
+        cleanDb((Session) entityManagerFactory.createEntityManager());
+    }
+
+    /**
+     * Pessimistic Lock :
+     * Block particular DB row for UPDATE, but allow to READ across different transactions
+     * if they aren't going to chang it next, using PESSIMISTIC_READ.
+     */
+    @Test
+    void lockRow_forUpdate_usingPessimisticReadLock() {
+        createDialogForCouple((Session) entityManagerFactory.createEntityManager(), DIALOG_ID);
+
         @Cleanup final Session session1 = (Session) entityManagerFactory.createEntityManager();
         @Cleanup final Session session2 = (Session) entityManagerFactory.createEntityManager();
 
-        session1.beginTransaction();
-        session2.beginTransaction();
+        //validate:
+        Exception ex = assertThrows(RollbackException.class, () -> {
+            session1.beginTransaction();
+            session2.beginTransaction();
 
-        try {
+            // U1 :
+            var dialog = session1.find(Dialog.class, 1, LockModeType.PESSIMISTIC_READ);
+            dialog.setName(F_D_CH);
 
+            session2.createNativeQuery("SET lock_timeout = '5000ms'").executeUpdate();
+            // U2 (будет заблокирован только для внесения изменений).
+            // Использую "SET lock_timeout" выше, потому что Postgres не восприимчив к LockOptions,
+            // "properties" параметер ниже предствален просто в качестве примера использования :
+            var theSamedialog = session2.find(Dialog.class, 1, LockModeType.PESSIMISTIC_READ,
+                    Map.of(JPA_LOCK_TIMEOUT, 5000));
+            theSamedialog.setName("Second dialog NAME changing");
 
-            //User 1:
-//            Message message = session1.createQuery(
-//                            "select m from Message m " +
-//                                    "join fetch m.author a " +
-//                                    "join fetch a.memberInfo m_inf " +
-//                                    "where m.messageId = :messageId ", Message.class)
-//                    .setParameter("messageId", MESSAGE_ID)
-//                    .setLockMode(LockModeType.PESSIMISTIC_READ)
-//                    .setHint(JPA_LOCK_TIMEOUT, 2000)
-//                    .getSingleResult();
-            LockOptions lockOptions = new LockOptions(LockMode.PESSIMISTIC_READ);
-            lockOptions.setTimeOut(1000);
-            Message message = session1.get(Message.class, 1, lockOptions);
-            message.setDescription(F_M_CH);
-
-            //User 2:
-            Message theSameMessage = session2.createQuery(
-                            "select m from Message m " +
-                                    "join fetch m.author a " +
-                                    "join fetch a.memberInfo m_inf " +
-                                    "where m.messageId = :messageId ", Message.class)
-                    .setParameter("messageId", MESSAGE_ID)
-                    .getSingleResult();
-            theSameMessage.setDescription("Second message DESCRIPTION changing");
-
-            //In this case just second user must waite until first will finish transaction:
+            // Исключение по таймауту будет выбрашено после следующей строчки кода
+            // (во время попытки применить изменения) :
             session2.getTransaction().commit();
             session1.getTransaction().commit();
+        });
 
-        } catch (Exception ex) {
-            System.out.println("dfgdf");
-        }
-
-        System.out.println("sdfg");
+        assertTrue(ex.getCause() instanceof javax.persistence.PessimisticLockException);
+        var dialog = readDialogByDialogIdWithFetchedMembers((Session) entityManagerFactory.createEntityManager(), DIALOG_ID);
+        assertEquals("name of test-dialog-1", dialog.getName());
     }
+
+    /**
+     * Pessimistic Lock :
+     * Block particular DB row for UPDATE and READ, using PESSIMISTIC_WRITE.
+     *
+     * No inner Entities.
+     */
+    @Test
+    void lockRow_forShareAndUpdate_usingPessimisticWriteLock() {
+        createDialogForCouple((Session) entityManagerFactory.createEntityManager(), DIALOG_ID);
+
+        @Cleanup final Session session1 = (Session) entityManagerFactory.createEntityManager();
+        @Cleanup final Session session2 = (Session) entityManagerFactory.createEntityManager();
+
+        //validate:
+        Exception ex = assertThrows(LockTimeoutException.class, () -> {
+            session1.beginTransaction();
+            session2.beginTransaction();
+
+            // U1 :
+            var dialog = session1.find(Dialog.class, 1, LockModeType.PESSIMISTIC_WRITE);
+            dialog.setName(F_D_CH);
+
+            // U2 (будет заблокирован даже для чтения) :
+            session2.createNativeQuery("SET lock_timeout = '5000ms'").executeUpdate();
+            // Запрос ниже пройдет успешно, т.к. участники диалога не запрашивались как вложеные сущности
+            // и на эти записи в БД не распространилось влияние блокировки :
+            var user1 = session2.find(Member.class, 1, LockModeType.PESSIMISTIC_READ);
+            // Исключение по таймауту будет выбрашено после следующей строчки кода :
+            var theSamedialog = session2.find(Dialog.class, 1, LockModeType.PESSIMISTIC_READ,
+                    Map.of(JPA_LOCK_TIMEOUT, 5000));
+            theSamedialog.setName("Second dialog NAME changing");
+
+            session2.getTransaction().commit();
+            session1.getTransaction().commit();
+        });
+
+        assertTrue(ex.getCause() instanceof PessimisticLockException);
+        var dialog = readDialogByDialogIdWithFetchedMembers((Session) entityManagerFactory.createEntityManager(), DIALOG_ID);
+        assertEquals("name of test-dialog-1", dialog.getName());
+    }
+
+    /**
+     * Pessimistic Lock :
+     * Block particular DB row for UPDATE and READ, using PESSIMISTIC_WRITE.
+     *
+     * Fetch inner Entities. As we can see inner Entities will be blocked as well.
+     */
+    @Test
+    void lockRow_forShareAndUpdate_usingPessimisticWriteLock_withFetchingInnerEntities() {
+        createDialogForCouple((Session) entityManagerFactory.createEntityManager(), DIALOG_ID);
+
+        @Cleanup final Session session1 = (Session) entityManagerFactory.createEntityManager();
+        @Cleanup final Session session2 = (Session) entityManagerFactory.createEntityManager();
+
+        //validate:
+        Exception ex = assertThrows(javax.persistence.PessimisticLockException.class, () -> {
+            session1.beginTransaction();
+            session2.beginTransaction();
+
+            // U1 :
+            var dialog = session1.createQuery(
+                            "select d from Dialog d " +
+                                    "join fetch d.dialogByMemberDetails dm " +
+                                    "join fetch dm.member m " +
+                                    "where d.id = :id", Dialog.class)
+                    .setParameter("id", 1)
+                    .setLockMode(LockModeType.PESSIMISTIC_WRITE)
+                    .getSingleResult();
+
+            // U2 (будет заблокирован даже для чтения) :
+            session2.createNativeQuery("SET lock_timeout = '5000ms'").executeUpdate();
+            // Исключение по таймауту будет выбрашено после следующей строчки кода,
+            // т.к. в данном случае лок распространяется и на участников диалога
+            // ( U1:    select .. join .. for update of
+            //              dialogbyme1_,
+            //              dialog0_,
+            //              member2_
+            // ):
+            var member1 = session2.find(Member.class, 1, LockModeType.PESSIMISTIC_READ);
+            var theSamedialog = session2.find(Dialog.class, 1, LockModeType.PESSIMISTIC_READ,
+                    Map.of(JPA_LOCK_TIMEOUT, 5000));
+            theSamedialog.setName("Second dialog NAME changing");
+
+            session2.getTransaction().commit();
+            session1.getTransaction().commit();
+        });
+
+        assertTrue(ex.getCause() instanceof PessimisticLockException);
+        var dialog = readDialogByDialogIdWithFetchedMembers((Session) entityManagerFactory.createEntityManager(), DIALOG_ID);
+        assertEquals("name of test-dialog-1", dialog.getName());
+    }
+
+
+
+
+
+        //User 1:
+    //            Message message = session1.createQuery(
+    //                            "select m from Message m " +
+    //                                    "join fetch m.author a " +
+    //                                    "join fetch a.memberInfo m_inf " +
+    //                                    "where m.messageId = :messageId ", Message.class)
+    //                    .setParameter("messageId", MESSAGE_ID)
+    //                    .setLockMode(LockModeType.PESSIMISTIC_READ)
+    //                    .setHint(JPA_LOCK_TIMEOUT, 2000)
+    //                    .getSingleResult();
+
+    //            LockOptions lockOptions = new LockOptions(LockMode.PESSIMISTIC_WRITE);
+    //            lockOptions.setTimeOut(1000);
+    //            Message message = session1.get(Message.class, 1, lockOptions);
+    //
+    //            message.setDescription(F_D_CH);
+    //
+    //
+    //
+
+        //User 2:
+    //            Message theSameMessage = session2.createQuery(
+    //                            "select m from Message m " +
+    //                                    "join fetch m.author a " +
+    //                                    "join fetch a.memberInfo m_inf " +
+    //                                    "where m.messageId = :messageId ", Message.class)
+    //                    .setParameter("messageId", MESSAGE_ID)
+    //                    .getSingleResult();
+    //            theSameMessage.setDescription("Second message DESCRIPTION changing");
 }
