e0a97862caa8636cf1b04365db91cb8693dbf7a6
aleh.vilchynski <vilchinskioleg@gmail.com>
2025-12-02 23:01:47 +0100

    Refactored algorithms.

--- auth-parent/auth-configuration/src/test/java/org/tms/common/auth/configuration/global/algorithms/let_code/HashMapUtil.java
+++ auth-parent/auth-configuration/src/test/java/org/tms/common/auth/configuration/global/algorithms/let_code/HashMapUtil.java
@@ -234,4 +234,32 @@ public class HashMapUtil {
 
     return new ArrayList<>(groups.values());
   }
+
+
+
+  /**
+   * 1. Two Sum
+   *
+   * Given an array of integers nums and an integer target, return indices of the two numbers such
+   * that they add up to target.
+   *
+   * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+   *
+   * You can return the answer in any order.
+   */
+
+  public static int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> digitIndBySurplus = new HashMap<>();
+
+    for (int i = 0; i < nums.length; i++) {
+      Integer firstMemberInd = digitIndBySurplus.get(nums[i]);
+      if (firstMemberInd == null) {
+        digitIndBySurplus.put(target - nums[i], i); // put "potential" firstMemberInd (as val) by "potential" (expected) secondMemberVal (as key)
+      } else {
+        return new int[] {firstMemberInd, i};
+      }
+    }
+
+    return new int[2]; // just MOCK
+  }
 }
