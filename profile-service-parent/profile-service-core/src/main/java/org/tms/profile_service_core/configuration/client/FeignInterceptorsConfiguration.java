34517bb27bd26f5bcfd2d419cbf155395bc91657
aleh.vilchynski <vilchinskioleg@gmail.com>
2026-02-26 13:22:40 +0100

    Refactored profile service notifications tools

--- profile-service-parent/profile-service-core/src/main/java/org/tms/profile_service_core/configuration/client/FeignInterceptorsConfiguration.java
+++ profile-service-parent/profile-service-core/src/main/java/org/tms/profile_service_core/configuration/client/FeignInterceptorsConfiguration.java
@@ -2,12 +2,10 @@ package org.tms.profile_service_core.configuration.client;
 
 import lombok.RequiredArgsConstructor;
 import org.common.http.autoconfiguration.model.CommonData;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.tms.common.auth.configuration.client.AuthRestClientProperties;
 import org.tms.profile_service_core.configuration.client.auth.AuthRestClientAuthInterceptor;
-import org.tms.profile_service_core.configuration.client.email.RapidEmailSenderApiInterceptor;
 
 /**
  * Can use that config class to create all needed Interceptor-Beans for different clients.
@@ -29,12 +27,5 @@ public class FeignInterceptorsConfiguration {
                 authRestClientProperties.getPassword(),
                 commonData
         );
-    }
-
-    @Bean
-    public RapidEmailSenderApiInterceptor rapidEmailSenderApiInterceptor(
-            @Value("${email-sender-open-api.host}") String host,
-            @Value("${email-sender-open-api.token}") String token) {
-        return new RapidEmailSenderApiInterceptor(host, token);
     }
 }
