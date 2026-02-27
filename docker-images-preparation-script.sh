17fe5bf960da40df8fb1e5d374ccef0aad2b84fb
aleh.vilchynski <vilchinskioleg@gmail.com>
2026-02-27 14:32:32 +0100

    Refactored CD pipeline

--- docker-images-preparation-script.sh
+++ docker-images-preparation-script.sh
@@ -9,6 +9,9 @@ docker push "${DOCKER_ID}"/kafka-ca-ssl
 docker build -t "${DOCKER_ID}"/kafka-ssl-broker1 -f ./kafka-ssl/Dockerfile.Kafka.SSL ./kafka-ssl
 docker push "${DOCKER_ID}"/kafka-ssl-broker1
 
+docker build -t "${DOCKER_ID}"/notification-gateway-service-boot ./notification-gateway-parent/notification-gateway-service
+docker push "${DOCKER_ID}"/notification-gateway-service-boot
+
 docker build -t "${DOCKER_ID}"/auth-service-boot ./auth-parent/auth-service-rest
 docker push "${DOCKER_ID}"/auth-service-boot
 
@@ -24,5 +27,5 @@ docker push "${DOCKER_ID}"/event-storage-service-boot
 docker build -t "${DOCKER_ID}"/message-chat-service-boot ./message-chat-service
 docker push "${DOCKER_ID}"/message-chat-service-boot
 
-docker build -t "${DOCKER_ID}"/task-executor-service-boot ./task-executor-service
-docker push "${DOCKER_ID}"/task-executor-service-boot
+#docker build -t "${DOCKER_ID}"/task-executor-service-boot ./task-executor-service
+#docker push "${DOCKER_ID}"/task-executor-service-boot
