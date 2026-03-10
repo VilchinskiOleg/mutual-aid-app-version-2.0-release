4b9f94fe0f44ff739f891bed0f0926a0c51cadfd
aleh.vilchynski <vilchinskioleg@gmail.com>
2026-03-10 13:44:35 +0100

    Refactored alg

--- auth-parent/auth-configuration/src/test/java/org/tms/common/auth/configuration/global/algorithms/let_code/MatrixUtil.java
+++ auth-parent/auth-configuration/src/test/java/org/tms/common/auth/configuration/global/algorithms/let_code/MatrixUtil.java
@@ -1,9 +1,64 @@
 package org.tms.common.auth.configuration.global.algorithms.let_code;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MatrixUtil {
+
+    /**
+     * 36. Valid Sudoku
+     *
+     * Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated
+     * according to the following rules:
+     *
+     * - Each row must contain the digits 1-9 without repetition.
+     * - Each column must contain the digits 1-9 without repetition.
+     * - Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+     *
+     * Note:
+     *
+     * - A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+     * - Only the filled cells need to be validated according to the mentioned rules.
+     */
+
+    public static boolean isValidSudoku(char[][] board) {
+        int [][] rowCache = new int [board.length][10];    // int[10] per each row index
+        int [][] columnCache = new int [board.length][10]; // int[10] per each column index
+        Map<String, int[]> squareCache = new HashMap<>();  // int[10] per each square token (String)
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                char value = board[i][j];
+
+                if (Character.isDigit(value)) { // OR: if(ch >= '0' && ch <= '9') {..}
+                    int number = Character.getNumericValue(value);
+                    // OR: int number = value - '0';
+
+                    // 1. Check and update Row Cache:
+                    if (rowCache[i][number] > 0) return false;
+                    rowCache[i][number]++;
+
+                    // 2. Check and update Column Cache:
+                    if (columnCache[j][number] > 0) return false;
+                    columnCache[j][number]++;
+
+                    // 3. Check and update Square Cache:
+                    int[] squareValues = squareCache.computeIfAbsent(
+                        String.valueOf(i) + j,
+                        k -> new int[10]);
+                    if (squareValues[number] > 0) return false;
+                    squareValues[number]++;
+                }
+            }
+
+        }
+
+        return true;
+    }
+
+
 
     /**
      * 54. Spiral Matrix
@@ -56,4 +111,5 @@ public class MatrixUtil {
 
         return result;
     }
+
 }
