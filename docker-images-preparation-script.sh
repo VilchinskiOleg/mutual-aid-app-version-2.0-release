55dfe800208323049edbe4772c18b379590b2919
aleh.vilchynski <vilchinskioleg@gmail.com>
2026-03-22 17:57:38 +0100

    Refactored sh file

--- docker-images-preparation-script.sh
+++ docker-images-preparation-script.sh
@@ -27,5 +27,6 @@ docker push "${DOCKER_ID}"/event-storage-service-boot
 docker build -t "${DOCKER_ID}"/message-chat-service-boot ./message-chat-service
 docker push "${DOCKER_ID}"/message-chat-service-boot
 
+
 #docker build -t "${DOCKER_ID}"/task-executor-service-boot ./task-executor-service
 #docker push "${DOCKER_ID}"/task-executor-service-boot
