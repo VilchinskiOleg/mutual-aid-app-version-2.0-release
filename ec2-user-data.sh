916fdbc1dbcf4538a22947ef9ac339864d22421e
VilchinskiOleg <vilchinskioleg@gmail.com>
2026-06-16 23:58:22 +0200

    Refactored files

--- ec2-user-data.sh
+++ ec2-user-data.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 mkdir -p /home/ec2-user/postgres/backup
-mkdir /home/ec2-user/kafka-ssl
-mkdir /home/ec2-user/kafka-avro
+
+if grep -q "Amazon Linux release 2" /etc/system-release; then
+  yum update -y
+  amazon-linux-extras install docker -y
+elif grep -q "Amazon Linux release 2023" /etc/system-release; then
+  dnf update -y
+  dnf install -y docker
+else
+  echo "Unsupported Amazon Linux version"
+  exit 1
+fi
+
+systemctl enable docker
+systemctl start docker
